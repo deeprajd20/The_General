da16739a075dee1c2c9f3f2da6f98b5e2710f1f8
deepraj <deeprajd71@gmail.com>
2025-12-06 12:21:43 +0530

    feat:add chat endpoint

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-import os
-from dotenv import load_dotenv
-from src.core.chat import Chatbot
 from fastapi import FastAPI
+from src.api.router.api_router import api_router
 
+app = FastAPI()
 
-load_dotenv()
-
-api_key = os.getenv("groq_api_key")
-
-bot = Chatbot(groq_api_key=api_key)
-
-response = bot.interact('what is database')
-print(response)
+app.include_router(api_router, prefix="/api")
---
+++ src/api/router/chat.py
@@ -0,0 +1,15 @@
+import os
+from fastapi import APIRouter
+from ...core.chat import Chatbot
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.getenv('groq_api_key')
+
+router = APIRouter()
+bot = Chatbot(groq_api_key=api_key)
+
+@router.get("/")
+def chat():
+    response = bot.interact('what is os')
+    return response
