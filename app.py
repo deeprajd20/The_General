b9ed0168624cb4b0ab3360e91865e13b0d4eae1c
deepraj <deeprajd71@gmail.com>
2025-12-06 19:35:57 +0530

    small fix

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from src.api.router.api_router import api_router
+import os
+from src.core.chat import Chatbot
 
-app = FastAPI()
+bot = Chatbot(groq_api_key=os.getenv("groq_api_key"))
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_methods=["*"],
-    allow_headers=["*"]
-)
-
-app.include_router(api_router, prefix="/api")
+while True:
+    text = input("You: ")
+    if text == "exit":
+        break
+    reply = bot.interact(text)
+    print("Bot:",reply['response'] )
--- main.py
+++ main.py
@@ -1,15 +1,3 @@
-# import os
-# from src.core.chat import Chatbot
-
-# bot = Chatbot(groq_api_key=os.getenv("groq_api_key"))
-
-# while True:
-#     text = input("You: ")
-#     if text == "exit":
-#         break
-#     reply = bot.interact(text)
-#     print("Bot:",reply['response'] )
-
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from src.api.router.api_router import api_router
