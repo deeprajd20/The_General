85a43483b8a2087c23f2c2edad994f88200d88cc
deepraj <deeprajd71@gmail.com>
2025-12-07 11:37:04 +0530

    feat: some good frontend template

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+
 from fastapi.middleware.cors import CORSMiddleware
-from src.api.router.api_router import api_router
-from src.api.router.home_router import router as home_router
+# from src.api.router.api_router import api_router
+# from api.router.home import router as home_router
+
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="src/frontend"), name="static")
 
 app.add_middleware(
     CORSMiddleware,
@@ -11,5 +16,14 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
-app.include_router(api_router, prefix="/api")
-app.include_router(home_router)
+# app.include_router(api_router, prefix="/api")
+# app.include_router(home_router)
+
+
+@app.get("/")
+def home():
+    return FileResponse("src/frontend/index.html")
+
+@app.get("/chat")
+def chat_page():
+    return FileResponse("src/frontend/chat.html")
